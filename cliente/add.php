9752410e736027768ec6db5d39465e17a31d96d9
Kennedy Alves <kennedyalves.ka@gmail.com>
2018-05-20 12:25:41 -0300

    Formulario de edicao implementando no de adição

--- cliente/add.php
+++ cliente/add.php
@@ -1,9 +1,12 @@
 <?php
-include_once('Cliente.php');
-include_once ('../Conexao.php');
 include_once ('../cabecalho.php');
+include_once('Cliente.php');
 
-$conexao = new Conexao();
+$cliente = new Cliente();
+
+if(!empty($_GET['id_cliente'])){
+    $cliente->carregarPorId($_GET['id_cliente']);
+}
 ?>
     <head>
 
@@ -97,37 +100,40 @@ $conexao = new Conexao();
     <h2>Novo Cliente</h2>
 
     <form class="form-horizontal" method="post" action="processamento.php?acao=salvar">
+
+        <input type="hidden" name="id_cliente" class="form-control" value="<?php echo $cliente->getIdCliente();?>">
+
         <!-- area de campos do form -->
         <hr />
         <div class="row">
             <div class="form-group col-md-7">
                 <label for="nome">Nome</label>
-                <input type="text" class="form-control" id="nome" name="nome" required>
+                <input type="text" class="form-control" value="<?php echo $cliente->getNome();?>" id="nome" name="nome" required>
             </div>
 
             <div class="form-group col-md-3">
                 <label for="cpf">CPF</label>
-                <input type="text" class="form-control" id="cpf" name="cpf" required>
+                <input type="text" class="form-control" value="<?php echo $cliente->getCpf();?>" id="cpf" name="cpf" required>
             </div>
             <div class="form-group col-md-2">
                 <label for="campo3">Data de Nascimento</label>
-                <input type="date" class="form-control" id="datanasci" name="datanasci" required>
+                <input type="date" class="form-control" value="<?php echo $cliente->getDatanasci();?>" id="datanasci" name="datanasci" required>
             </div>
 
         </div>
         <div class="row">
             <div class="form-group col-md-7">
                 <label for="email">Email</label>
-                <input type="email" class="form-control" id="email" name="email" required>
+                <input type="email" class="form-control" value="<?php echo $cliente->getEmail();?>" id="email" name="email" required>
             </div>
 
             <div class="form-group col-md-3">
                 <label for="rg">RG</label>
-                <input type="text" class="form-control" id="rg" name="rg" required>
+                <input type="text" class="form-control" value="<?php echo $cliente->getRg();?>" id="rg" name="rg" required>
             </div>
             <div class="form-group col-md-2">
                 <label for="telefone">Telefone</label>
-                <input type="text" class="form-control" id="telefone" name="telefone" required>
+                <input type="text" class="form-control" value="<?php echo $cliente->getTelefone();?>" id="telefone" name="telefone" required>
             </div>
 
         </div>
@@ -135,7 +141,7 @@ $conexao = new Conexao();
         <div class="row">
             <div class="form-group col-md-4">
                 <label for="celular">Celular</label>
-                <input type="text" class="form-control" id="celular" name="celular" required>
+                <input type="text" class="form-control" value="<?php echo $cliente->getCelular();?>" id="celular" name="celular" required>
             </div>
             <div class="form-group col-md-4">
                 <label for="sexo" class="col-sm-2 control-label">Sexo</label><br/>
@@ -156,16 +162,16 @@ $conexao = new Conexao();
         <div class="row">
             <div class="form-group col-md-4">
                 <label for="cep">CEP</label>
-                <input class="form-control" name="cep" type="text" id="cep" value=""
+                <input class="form-control" value="<?php echo $cliente->getCep();?>" name="cep" type="text" id="cep" value=""
                        onblur="pesquisacep(this.value);" required />
             </div>
             <div class="form-group col-md-4">
                 <label for="logradouro">Logradouro</label>
-                <input type="text" class="form-control" id="logradouro" name="logradouro" readonly>
+                <input type="text" class="form-control" value="<?php echo $cliente->getLogradouro();?>" id="logradouro" name="logradouro" readonly>
             </div>
             <div class="form-group col-md-4">
                 <label for="bairro">Bairro</label>
-                <input type="text" class="form-control" id="bairro" name="bairro" readonly>
+                <input type="text" class="form-control" value="<?php echo $cliente->getBairro();?>" id="bairro" name="bairro" readonly>
             </div>
 
         </div>
@@ -173,11 +179,11 @@ $conexao = new Conexao();
         <div class="row">
             <div class="form-group col-md-4">
                 <label for="cidade">Cidade</label>
-                <input type="text" class="form-control" id="cidade" name="cidade" readonly>
+                <input type="text" class="form-control" value="<?php echo $cliente->getCidade();?>" id="cidade" name="cidade" readonly>
             </div>
             <div class="form-group col-md-4">
                 <label for="uf">UF</label>
-                <input type="text" class="form-control" id="uf" name="uf" readonly>
+                <input type="text" class="form-control" value="<?php echo $cliente->getUf();?>" id="uf" name="uf" readonly>
             </div>
             <div class="form-group col-md-4">
                 <label for="ibge">IBGE</label>
@@ -189,11 +195,11 @@ $conexao = new Conexao();
         <div class="row">
             <div class="form-group col-md-4">
                 <label for="numero">Número</label>
-                <input type="text" class="form-control" id="numero" name="numero" required>
+                <input type="text" class="form-control" value="<?php echo $cliente->getNumero();?>" id="numero" name="numero" required>
             </div>
             <div class="form-group col-md-4">
                 <label for="complemento">Complemento</label>
-                <input type="text" class="form-control" id="complemento" name="complemento" required>
+                <input type="text" class="form-control" value="<?php echo $cliente->getComplemento();?>" id="complemento" name="complemento" required>
             </div>
 
         </div>
