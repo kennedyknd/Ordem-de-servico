871ebcc42676965b11c7fd4d5cfcf3ec83670755
Kennedy Alves <kennedyalves.ka@gmail.com>
2018-05-20 12:27:43 -0300

    Formulario de edicao implementando no de adição

--- servico/add.php
+++ servico/add.php
@@ -1,5 +1,12 @@
 <?php
 include_once('../cabecalho.php');
+include_once 'Servico.php';
+
+$servico = new Situacao();
+
+if(!empty($_GET['id_servico'])){
+    $servico->carregarPorId($_GET['id_servico']);
+}
 ?>
 
     <head>
@@ -11,17 +18,20 @@ include_once('../cabecalho.php');
     <h2>Novo Serviço</h2>
 
     <form class="form-horizontal" method="post" action="processamento.php?acao=salvar">
+
+        <input type="hidden" name="id_servico" class="form-control" value="<?php echo $servico->getIdServico();?>">
+
         <!-- area de campos do form -->
         <hr />
         <div class="row">
             <div class="form-group col-md-6">
                 <label for="servico">Serviço</label>
-                <input type="text" class="form-control" id="servico" name="servico" required>
+                <input type="text" class="form-control" value="<?php echo $servico->getServico();?>" id="servico" name="servico" required>
             </div>
 
             <div class="form-group col-md-6">
                 <label for="observacao">Observação</label>
-                <input type="text" class="form-control" id="observacao" name="observacao" required>
+                <input type="text" class="form-control" value="<?php echo $servico->getObservacao();?>" id="observacao" name="observacao" required>
             </div>
 
         </div>
