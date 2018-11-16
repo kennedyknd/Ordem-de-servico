4152eac4f7c73004b51a0f1e66fc5bd549571cf3
Kennedy Alves <kennedyalves.ka@gmail.com>
2018-11-16 17:54:05 -0200

    [UPDATE] Login

--- usuario/login.php
+++ usuario/login.php
@@ -3,11 +3,8 @@
 
         <form class="form-signin" action="processamento.php?acao=logar" method="post">
           <div class="panel periodic-login">
-              <span class="atomic-number">28</span>
               <div class="panel-body text-center">
-                  <h1 class="atomic-symbol">Mi</h1>
-                  <p class="atomic-mass">14.072110</p>
-                  <p class="element-name">Miminium</p>
+                  <h1 class="atomic-symbol">Login</h1>
 
                   <i class="icons icon-arrow-down"></i>
                   <div class="form-group form-animate-text" style="margin-top:40px !important;">
@@ -20,15 +17,11 @@
                     <span class="bar"></span>
                     <label>Senha</label>
                   </div>
-                  <label class="pull-left">
-                  <input type="checkbox" class="icheck pull-left" name="checkbox1"/> Remember me
+                  <label class="">
+                  <input type="checkbox" class="icheck pull-left" name="checkbox1"/> Lembrar senha
                   </label>
-                  <input type="submit" class="btn col-md-12" value="SignIn"/>
+                  <input type="submit" class="btn col-md-12" value="Entrar"/>
               </div>
-                <div class="text-center" style="padding:5px;">
-                    <a href="forgotpass.html">Forgot Password </a>
-                    <a href="reg.html">| Signup</a>
-                </div>
           </div>
         </form>
 
